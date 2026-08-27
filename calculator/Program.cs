1c340842b2ae5ee63eff7049bbe0c450f476b335
Safty952003 <safty952003@gmail.com>
2026-08-27 23:46:09 +0300

    feat: add basic calculator

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operation (+, -, *, /): ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    return;
+            }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
